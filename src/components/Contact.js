d53114b89e1624c4eb3867c3a463bf4670c6ca94
JoeBroski <100441472+JoeBroski@users.noreply.github.com>
2023-01-08 19:22:01 +0100

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,131 @@
+import React ,{useState} from "react";
+//import {AiFillTwitterCircle} from 'react-icons/ai';
+import { BsGithub,BsLinkedin } from "react-icons/bs";
+export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "test", name, email, message }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
+  }
+
+  return (
+    <section id="contact" className="relative">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "opacity(0.7)" }}
+            src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.1702509858724!2d-1.8985282848124787!3d34.65040308044732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd787cbdbf96caef%3A0x23a99f5d4e9a3161!2s%C3%89cole%20Nationale%20des%20Sciences%20Appliqu%C3%A9es!5e0!3m2!1sfr!2sma!4v1673180241026!5m2!1sfr!2sma"
+          />
+
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1">
+                 BP 669 Bd Mohammed VI AL Qods. <br/>
+                Oujda, Angad 60000
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a className="text-indigo-400 leading-relaxed">
+                Youssef.Ammari@gmail.com
+              </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                PHONE
+              </h2>
+              <p className="leading-relaxed">+212 6-45-67-89-10</p>
+            </div>
+          </div>
+        </div>
+        <form
+          netlify
+          name="test"
+          onSubmit={handleSubmit}
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Hire Me
+          </h2>
+          
+          <p className="leading-relaxed mb-5">
+           You can contact me through Linkdin or e-mail stated below .Also feel free to check the other projects of mine in my github account .
+          </p>
+           <div className="text-5xl flex justify-center  gap-10 ">
+             <a href="https://www.linkedin.com/in/youssef-ammari-9b8438193/" 
+                target="_blank" ><BsLinkedin /> </a>
+              <a href="https://github.com/JoeBroski" target="_blank"><BsGithub/></a>  
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-400">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const projects = [
+  {
+    title: "MuscleStudio",
+    subtitle: "Android Java/kotlin",
+    description:
+      "Manage and keep your own gym on track by using this app for registring and exposing services and offers to your clients .",
+    image: "./project-1.png",
+    link: "#projects",
+  },
+  
+  {
+    title: "React Todo-List App",
+    subtitle: "React Hooks",
+    description:
+      "Web application developed in react js to keep you oraganised and productive during the day  ",
+    image: "./project-2.png",
+    link: "#projects",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "I would like to express my satisfaction after using MuscleStudio App, I was fascinated with how efficient smooth and reliable that app is in running my fitness gym. I’m glad I decided to work with you. It’s really great how easy your apps are to manage and maintain. It was a great user experiment for me . ",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Belhadri Ziad",
+    company: "Kovelis",
+  },
+  {
+    quote:
+      "“I just wanted to share a quick note and let you know that you guys do a really good job. I’m glad I decided to work with you. It’s really great how easy your websites are to update and manage. I never have any problem at all.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Mazi Ilyas",
+    company: "NoSqli",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React js",
+  "C#",
+  "Java JEE (Spring Boot)",
+  "Android devlopment (Java Kotlin)",
+  "php",
+  ,"TailWind"
+  ,"Angular"
+];
